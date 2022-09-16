178d9a8d8a8ca9b89e2adeae28693a080baafbba
takitazwar.parthib <takitazwar.parthib@bjitgroup.com>
2022-09-16 11:21:04 +0600

    Update database.js

--- config/database.js
+++ config/database.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 const dbConnect = async (callback)=>{
     try 
     {
-        const client = await mongoose.connect(process.env.DATABASE_CONNECTION);
+        const client = await mongoose.connect("mongodb+srv://tgop:01711188881@Me@mongolearn.sdong.mongodb.net/academy_project?retryWrites=true&w=majority");
         if(client)
         {
             console.log("Database is Connected");
